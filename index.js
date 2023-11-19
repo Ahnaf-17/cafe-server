a5cb3f96892028fe9ff3c100b680ca64a1a9e3bc
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-19 15:55:52 +0600

    delete api created

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ app.use(express.json())
 
 
 
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.fz8oxax.mongodb.net/?retryWrites=true&w=majority`;
 
 // Create a MongoClient with a MongoClientOptions object to set the Stable API version
@@ -30,29 +30,79 @@ async function run() {
     const menuCollection = client.db('cafeDB').collection('menu')
     const reviewCollection = client.db('cafeDB').collection('reviews')
     const cartCollection = client.db('cafeDB').collection('cart')
+    const userCollection = client.db('cafeDB').collection('users')
+
+    // user 
+    app.post('/users', async (req, res) => {
+      const user = req.body;
+      const query = { email: user.email }
+      const existUser = await userCollection.findOne(query)
+      if (existUser) {
+        return res.send({ message: 'user exist' })
+      }
+      const result = await userCollection.insertOne(user);
+      res.send(result)
+    })
+    app.get('/users', async (req, res) => {
+      const result = await userCollection.find().toArray();
+      res.send(result)
+    })
+    app.delete('/users/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await userCollection.deleteOne(query);
+      res.send(result)
+    })
+
+    app.patch('/users/admin/:id', async(req,res)=>{
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)};
+      const updatedDoc = {
+        $set: {
+          role: 'admin'
+        }
+      }
+      const result = await userCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+    })
+
+
+
+
+
+
 
     // menu 
-    app.get('/menu',async(req,res)=>{
-        const result = await menuCollection.find().toArray();
-        res.send(result)
+    app.get('/menu', async (req, res) => {
+      const result = await menuCollection.find().toArray();
+      res.send(result)
     })
 
     // reviews 
-    app.get('/reviews',async(req,res)=>{
-        const result = await reviewCollection.find().toArray();
-        res.send(result)
+    app.get('/reviews', async (req, res) => {
+      const result = await reviewCollection.find().toArray();
+      res.send(result)
     })
 
     // cart 
-    app.post('/carts', async(req,res)=>{
+    app.post('/carts', async (req, res) => {
       const cartItem = req.body;
       const result = await cartCollection.insertOne(cartItem)
       res.send(result)
     })
 
-    app.get('/carts',async(req,res)=>{
-      const result = await cartCollection.find().toArray();
+    app.get('/carts', async (req, res) => {
+      const email = req.query.email
+      const query = { email: email }
+      const result = await cartCollection.find(query).toArray();
       res.send(result);
+    })
+
+    app.delete('/carts/:id', async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await cartCollection.deleteOne(query);
+      res.send(result)
     })
 
 
@@ -68,9 +118,9 @@ run().catch(console.dir);
 
 
 
-app.get('/', (req,res)=>{
-    res.send('cafe server running')
+app.get('/', (req, res) => {
+  res.send('cafe server running')
 })
-app.listen(port,()=>{
-    console.log(`cafe running on port : ${port}`);
+app.listen(port, () => {
+  console.log(`cafe running on port : ${port}`);
 })
