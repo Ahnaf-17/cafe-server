59855bbd0b3a042d0fc4801df3b6800379279ee9
unknown <ahnaf.ahmed.42173@gmail.com>
2023-11-22 22:07:53 +0600

    Send Price and Get client Secret from stripe

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ const app = express();
 const cors = require('cors');
 var jwt = require('jsonwebtoken');
 require('dotenv').config()
+const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY)
 const port = process.env.PORT || 5000;
 
 app.use(cors())
@@ -136,6 +137,41 @@ async function run() {
       res.send(result)
     })
 
+    app.get('/menu/:id',async (req,res)=>{
+      const id = req.params.id;
+      const query = {_id: new ObjectId(id)}
+      const result = await menuCollection.findOne(query)
+      res.send(result);
+    })
+
+    app.patch('/menu/:id',async(req,res)=>{
+      const item = req.body;
+      const id = req.params.id;
+      const filter = {_id: new ObjectId(id)}
+      const updatedDoc = {
+        $set: {
+          name: item.name,
+          category: item.category,
+          price: item.price,
+          recipe: item.recipe,
+          image: item.image
+        }
+      }
+      const result = await menuCollection.updateOne(filter,updatedDoc)
+      res.send(result)
+
+    })
+
+    app.delete('/menu/:id',verifyToken,verifyAdmin, async (req, res) => {
+      const id = req.params.id;
+      const query = { _id: new ObjectId(id) };
+      const result = await menuCollection.deleteOne(query);
+      res.send(result)
+    })
+
+ 
+
+
     // reviews 
     app.get('/reviews', async (req, res) => {
       const result = await reviewCollection.find().toArray();
@@ -161,6 +197,21 @@ async function run() {
       const query = { _id: new ObjectId(id) };
       const result = await cartCollection.deleteOne(query);
       res.send(result)
+    })
+
+    // payment 
+    app.post('/create-payment-intent',async(req,res)=>{
+      const {price} = req.body;
+      const amount = parseInt(price*100);
+      const paymentIntent = await stripe.paymentIntents.create({
+        amount: amount,
+        currency: 'usd',
+        payment_method_types: ['card']  
+
+      })
+      res.send({
+        clientSecret: paymentIntent.client_secret
+      })
     })
 
 
